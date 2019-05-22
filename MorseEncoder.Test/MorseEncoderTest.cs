953ce83d4cd87e68f5efc34a70f63f13a33a8d2c
Victor Garces <victorgarcesgg@gmail.com>
2019-05-22 16:05:50 -0400

    Encode one word to morse code

--- MorseEncoder.Test/MorseEncoderTest.cs
+++ MorseEncoder.Test/MorseEncoderTest.cs
@@ -3,8 +3,25 @@
 namespace MorseEncoder.Test
 {
     [TestFixture]
-    public class MorseEncoderTest
+    public sealed class MorseEncoderTest
     {
+
+        /*
+         * 
+         */
+
+        [TestCase("E", ".")]
+        [TestCase("", "")]
+        [TestCase("s", "...")]
+        [TestCase("sos", "...---...")]
+        [TestCase("hoLA", "....---.-...-")]
+        public void Given_String_Return_Morse_Code(string input, string output)
+        {
+            MorseEncoder morseEncoder = new MorseEncoder();
+            string morseCode = morseEncoder.TextToMorse(input);
+
+            Assert.That(morseCode, Is.EqualTo(output));
+        }
 
     }
 }
--- MorseEncoder/MorseEncoder.cs
+++ MorseEncoder/MorseEncoder.cs
@@ -1,6 +1,57 @@
-﻿namespace MorseEncoder
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MorseEncoder
 {
-    public class MorseEncoder
+    public sealed class MorseEncoder
     {
+        private static readonly Dictionary<char, string> MorseDictionary = new Dictionary<char, string>
+        {
+           {'a', ".-"},
+           {'b', "-..."},
+           {'c', "-.-."},
+           {'d', "-.."},
+           {'e', "."},
+           {'f', "..-."},
+           {'g', "--."},
+           {'h', "...."},
+           {'i', ".."},
+           {'j', ".---"},
+           {'k', "-.-"},
+           {'l', ".-.."},
+           {'m', "--"},
+           {'n', "-."},
+           {'o', "---"},
+           {'p', ".--."},
+           {'q', "--.-"},
+           {'r', ".-."},
+           {'s', "..."},
+           {'t', "-"},
+           {'u', "..-"},
+           {'v', "...-"},
+           {'w', ".--"},
+           {'x', "-..-"},
+           {'y', "-.--"},
+           {'z', "--.."},
+           {'0', "-----"},
+           {'1', ".----"},
+           {'2', "..---"},
+           {'3', "...--"},
+           {'4', "....-"},
+           {'5', "....."},
+           {'6', "-...."},
+           {'7', "--..."},
+           {'8', "---.."},
+           {'9', "----."}
+        };
+
+        public string TextToMorse(string input)
+        {
+            string[] morseArray = input.ToLower()
+                .Select(character => MorseDictionary[character]).ToArray();
+            string morseEncoded = string.Join("", morseArray);
+
+            return morseEncoded;
+        }
     }
 }
