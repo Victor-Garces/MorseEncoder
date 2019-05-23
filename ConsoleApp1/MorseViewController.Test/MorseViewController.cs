f24b6c25002ae69b0476f4c9e1c7f5e7235b2d4b
Raul Caminero <r.caminero@wepsys.com.do>
2019-05-22 20:34:38 -0400

    Fixed conflicts.

---
+++ ConsoleApp1/MorseViewController.Test/MorseViewController.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using ConsoleApp1;
+
+namespace MorseViewController.Test
+{
+    [TestFixture]
+    public class MorseViewTest
+    {
+        [Test]
+        public void When_Pass_Args_To_Encoder_Works()
+        {
+
+            MorseViewController morseViewController = new MorseViewController([""]);
+
+            morseViewController.PassDataToEncoder();
+        }
+    }
+}
