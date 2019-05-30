f11c69d49c900dae07364b97cb63e821c4cea9dc
Victor Garces <victorgarcesgg@gmail.com>
2019-05-30 11:21:10 -0400

    Finished Beeping Class Functionality

--- Beeper.Test/BeeperTest.cs
+++ Beeper.Test/BeeperTest.cs
@@ -1,26 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Beeper.Test
 {
-
     [TestFixture]
     public sealed class BeeperTest
     {
+        private static int _timeUnit = 500;
+        private static int _rangeGap = 5;
+
+
         [TestCase(3, 1500)]
         public void Given_One_Time_Unit_Beep(int times, int output)
         {
-            const int timeUnit = 500;
-            Beeper beeper = new Beeper(timeUnit);
+            Beeper beeper = new Beeper(_timeUnit);
 
             long beep = beeper.Beep(times);
 
-            Assert.That(beep, Is.InRange(output, output));
+            Assert.That(beep, Is.InRange(output - _rangeGap, output + _rangeGap));
+        }
+
+        [TestCase("...",2500)]
+        [TestCase("|", 1500)]
+        [TestCase(" ", 3500)]
+        [TestCase("...|", 4000)]
+        [TestCase("...|...", 6500)]
+        [TestCase("...|---|...", 13500)]
+        [TestCase("...|---|... ...|---|...", 30500)]
+        public void Given_A_Morse_Encoded_Beep_Code(string input, int output)
+        {
+            Beeper beeper = new Beeper(_timeUnit);
+            long time = beeper.MorseToBeep(input);
+
+            Assert.That(time, Is.InRange(output - _rangeGap, output + _rangeGap));
         }
     }
 }
--- Beeper/Beeper.cs
+++ Beeper/Beeper.cs
@@ -1,25 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace Beeper
 {
     public class Beeper
     {
         private readonly int _timeUnit;
-
-        public Beeper(int timeUnit)
+        private static Dictionary<string, int> _dictionary = new Dictionary<string, int>
         {
-            _timeUnit = timeUnit;
-        }
+            {".", 1},
+            {"-", 3 },
+            {" ", 7},
+            {"|", 3}
+        };
+
+        public Beeper(int timeUnit) => _timeUnit = timeUnit;
 
         public long Beep(int times)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Console.Beep(800, times*_timeUnit);
+            const int frequency = 800;
+            Console.Beep(frequency, times * _timeUnit);
             stopwatch.Stop();
 
             return stopwatch.ElapsedMilliseconds;
+        }
+
+        public long MorseToBeep(string morseCode)
+        {
+            long sum = 0;
+
+            foreach (var item in morseCode.Select((value, i) => (value, i)))
+            {
+                char currentCharacter = item.value;
+                const int nextCharacterPosition = 1;
+                int index = item.i + nextCharacterPosition;
+                int times = _dictionary[currentCharacter.ToString()];
+
+                if (currentCharacter != ' ' && currentCharacter != '|')
+                {
+                    sum += Beep(times);
+
+                    if (index < morseCode.Length && (morseCode[index] != ' ' && morseCode[index] != '|'))
+                    {
+                        const int unitTime = 1;
+                        sum += GoToSleep(unitTime);
+                    }
+                }
+                else
+                {
+                    sum += GoToSleep(times);
+                }
+            }
+
+            return sum;
+        }
+
+        private int GoToSleep(int times)
+        {
+            int time = times * _timeUnit;
+            Thread.Sleep(time);
+
+            return time;
         }
     }
 }
